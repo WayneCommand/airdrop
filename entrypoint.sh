54fc0ed46aaec5e4ae2be32e6d44700c3ece21ec
WayneCommand <wayne@waynecommand.com>
2024-04-06 03:43:51 +0000

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# setting permission.
-chown -R ${PUID}:${PGID} /app/airdrop
-umask ${UMASK}
-
 # start server 10000
 service nginx restart
 echo 'web server started!'
@@ -11,6 +7,5 @@ echo 'web server started!'
 if [ "$1" = "version" ]; then
   echo 'wayne airdrop 1.0'
 else
-#  exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
-  cd /app/airdrop/server; node index.js
+  cd snapdrop/server; node index.js
 fi
